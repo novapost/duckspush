5f52d7294d5cbd76da531bbfad73280e932ca199
Jonathan Dorival <jonathan.dorival@novapost.fr>
2013-07-31 17:44:15 +0200

    Add some exceptions

--- exc.py
+++ exc.py
@@ -1,12 +1,22 @@
 class ApiDoesNotExist(Exception):
     pass
 
+
 class CollectorDoesNotExist(Exception):
-    
     def __init__(self, func_name, widget):
         self.func_name = func_name
         self.widget = widget
 
     def __str__(self):
         return "Collector: \"%s\" not found for Widget: %s" % (self.func_name,
-                                                              self.widget)
+                                                               self.widget)
+
+
+class CollectorProjectDoesNotExist(Exception):
+    def __str__(self):
+        return "Please start a collector project before runing publisher"
+
+
+class PublisherSettingsDoesNotExist(Exception):
+    def __str__(self):
+        return "Unable to find the publisher settings file"
